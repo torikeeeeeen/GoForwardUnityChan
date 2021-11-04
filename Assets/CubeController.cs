94a0e2b1e338cb6c2075b7d628bed849fa8d0bc0
torikeeeeeen <torikeentorikeen@gmail.com>
2021-11-04 17:38:42 +0900

    キューブ接触時の効果音追加
    CubeControllerに、CubeとGround/Cube接触時にSEが入るよう改修

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -11,11 +11,19 @@ public class CubeController : MonoBehaviour
     // 消滅位置
     private float deadLine = -10;
 
+    // ブロック衝突時の効果音
+    public AudioClip SE;
+
+    // AudioSource格納用
+    AudioSource audiosource;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        this.audiosource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -29,6 +37,17 @@ public class CubeController : MonoBehaviour
         if(transform.position.x < this.deadLine)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        // 発展：GroundまたはCubeと衝突したときに効果音を鳴らす
+        if (other.gameObject.tag == "GroundTag" || other.gameObject.tag == "CubeTag")
+        {
+            // 音を一回だけ再生する（ボリュームは3割で）
+            this.audiosource.PlayOneShot(SE,0.3F);
         }
     }
 }
